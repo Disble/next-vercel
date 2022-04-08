62310ebb660a2bc9eee1910be5bc1f76158a5dc4
Disble <decoder@hotmail.es>
2022-04-08 08:23:03 -0500

    21 Tarea de la seccion

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,12 +2,33 @@ import Link from "next/link";
 import { ActiveLink } from "./ActiveLink";
 import styles from './NavBar.module.css';
 
+const menuItems = [
+  {
+      text: 'Home',
+      href: '/'
+  },
+  {
+      text: 'About',
+      href: '/about'
+  },
+  {
+      text: 'Contact',
+      href: '/contact'
+  },
+  {
+      text: 'Pricing',
+      href: '/pricing'
+  },
+];
+
 export const NavBar = () => {
   return (
     <nav className={ styles['menu-container'] }>
-      <ActiveLink text="Home" href="/" />
-      <ActiveLink text="About" href="/about" />
-      <ActiveLink text="Contact" href="/contact" />
+      {
+        menuItems.map(({ href, text }) => (
+          <ActiveLink key={`link-${href}`} text={text} href={href} />
+        ))
+      }
     </nav>
   )
 }
